alaviss/nim.nvim

commit dc868703122cfe39d52a0d192ab24b7cbcbbd878

syntax/nim: improved pragma highlighting
- {.emit.}, {.codegenDecl.} can now highlight the passed code
  - By default the highlighter will apply C syntax on the associated
    code blocks. Users can change this by setting `g:nim_emit_syntax` to
    the backend of their choice. Supported choices are:
      - c
      - cpp
      - js
      - objc

    Alternatively the path to a syntax file can be passed directly.

- Pragma lists now have correct highlighting for Nim's syntax.

indent/nim.vim
@@ -38,8 +38,10 @@ endfunction
 " check if the given cursor position is a part of indent-insignificant syntax
 " groups.
 function! s:ignorePos(lnum, col) abort
-  return s:syntaxName(a:lnum, a:col) =~# '\%(' .. s:CommentGroups .. '\|'
-        \.. s:StringGroups .. '\|' .. s:CharGroups .. '\)'
+  let syntax = s:syntaxName(a:lnum, a:col)
+  return syntax !~# '^nim' &&
+        \syntax =~# '\%(' .. s:CommentGroups .. '\|' .. s:StringGroups ..
+        \           '\|' .. s:CharGroups .. '\)'
 endfunction
 
 " get the byte indices containing the beginning and end of the comments on the

syntax/nim.vim
@@ -10,6 +10,19 @@ if exists("b:current_syntax")
   finish
 endif
 
+let chosen_syntax = get(g:, 'nim_emit_syntax', 'c')
+if chosen_syntax is? 'c'
+  syntax include @emitSyntax syntax/c.vim
+elseif chosen_syntax =~? 'cpp\|c++'
+  syntax include @emitSyntax syntax/cpp.vim
+elseif chosen_syntax =~? 'js\|javascript'
+  syntax include @emitSyntax syntax/javascript.vim
+elseif chosen_syntax is? 'objc'
+  syntax include @emitSyntax syntax/objc.vim
+else
+  execute 'syntax include @emitSyntax ' .. chosen_syntax
+endif
+
 let b:current_syntax = "nim"
 
 " Keyword from the Manual, classified to different categories
@@ -31,11 +44,14 @@ syntax keyword nimStructure       enum object tuple
 
 syntax keyword nimPreProcStmt     alignof compiles defined sizeof
 
+syntax cluster nimKeywordGroup contains=nimKeywordOperator,nimStatement,nimConditional,nimException,nimRepeat,nimConstant,nimPreCondit,nimInclude,nimStructure,nimPreProcStmt
+
 syntax match nimComment      "#.*$" contains=nimTodo,@Spell
 syntax region nimLongDocComment start='##\[' end=']##' contains=nimTodo,nimLongDocComment,@Spell
 syntax region nimLongComment start='#\[' end=']#' contains=nimTodo,nimLongComment,nimLongDocComment,@Spell
-syntax cluster nimCommentGroup contains=nimComment,nimLongDocComment,nimLongComment
 syntax keyword nimTodo       FIXME NOTE NOTES TODO XXX contained
+
+syntax cluster nimCommentGroup contains=nimComment,nimLongDocComment,nimLongComment
 
 syntax region nimEscapedSymbol
       \ matchgroup=nimBacktick
@@ -82,6 +98,8 @@ syntax match nimFloat display "\<\d\+\%(_\d\+\)*\%(\.\d\+\%(_\d\+\)*\)\%([eE][+-
 " if have exponent
 syntax match nimFloat display "\<\d\+\%(_\d\+\)*\%([eE][+-]\=\d\+\%(_\d\+\)*\)\%('\=\%([fF]\%(32\|64\)\=\|[dD]\)\)\=\>"
 
+syntax cluster nimLiteralGroup contains=nimString,nimRawString,nimCharacter,nimNumber,nimFloat
+
 " pragmas in Nim are matched style-insensitively, so we need to create custom
 " matches for them.
 function! s:matchPragmas(pragmas) abort
@@ -110,9 +128,41 @@ call s:matchPragmas(['deprecated', 'noSideEffect', 'compileTime', 'noReturn',
      \               'inject', 'gensym', 'explain', 'noRewrite', 'package',
      \               'inheritable', 'constructor'])
 
+syntax region nimEmitString
+      \ matchgroup=nimQuote
+      \ start=+"+ skip=+\\"+ end=+"+
+      \ contains=@emitSyntax,nimEscapeStr,nimEscapeChar,nimEscapeQuote
+      \ oneline
+      \ contained
+      \ keepend
+syntax region nimEmitRawString
+      \ matchgroup=nimQuote
+      \ start='\<\K\k*"' end='"'
+      \ contains=@emitSyntax
+      \ oneline
+      \ contained
+      \ keepend
+syntax region nimEmitRawString
+      \ matchgroup=nimTripleQuote
+      \ start='\%(\<\K\k*\)\?"""' end='"*\zs"""'
+      \ contains=@emitSyntax
+      \ contained
+      \ keepend
+
+syntax region nimEmitArray
+      \ start='\[' end=']'
+      \ contains=@nimKeywordGroup,@nimLiteralGroup,@nimCommentGroup,nimEmitString,nimEmitRawString
+      \ contained
+
+execute 'syntax region nimEmit ' ..
+      \ 'start=+\<\%(' .. nim#SearchIdentifierRegex('emit') .. '\|' .. nim#SearchIdentifierRegex('codegendecl') .. '\)\m\s*:+ ' ..
+      \ 'end=+\ze\%(,\|\.\?}\)+ ' ..
+      \ 'contains=@nimKeywordGroup,@nimLiteralGroup,@nimCommentGroup,nimPragma,nimEmit.\+ ' ..
+      \ 'contained'
+
 syntax region nimPragmaList
       \ start=+{\.+ end=+\.\?}+
-      \ contains=nimPragma,nimString,nimRawString
+      \ contains=@nimKeywordGroup,@nimLiteralGroup,@nimCommentGroup,nimPragma,nimEmit
 
 " sync at the beginning of functions definition
 syntax sync match nimSync grouphere NONE "^\%(proc\|func\|iterator\|method\)\s\+\a\w*\s*[(:=]"

tests/tmixed.nim
@@ -1,0 +1,5 @@
+{.emit: """
+if (true)
+""".}
+
+{ #! -1

commit 60f42f4d5669d562e8fd513ccecf5722895b2b08

indent/nim: don't ignore line without syntax
Not having a syntax is not a problem if the line is not empty (ie.
tokens like whitespace).

Fix regression introduced by dc868703122cfe39d52a0d192ab24b7cbcbbd878

indent/nim.vim
@@ -39,7 +39,7 @@ endfunction
 " groups.
 function! s:ignorePos(lnum, col) abort
   let syntax = s:syntaxName(a:lnum, a:col)
-  return syntax !~# '^nim' &&
+  return (syntax isnot# '' && syntax !~# '^nim') ||
         \syntax =~# '\%(' .. s:CommentGroups .. '\|' .. s:StringGroups ..
         \           '\|' .. s:CharGroups .. '\)'
 endfunction

tests/tcomment.nim
@@ -1,0 +1,2 @@
+# if smt:
+#! -1